8dec3b51d172cbbe55970a8027cc85bf88134991
bransonenani <bransonenani@ucsb.edu>
2023-10-04 17:31:28 -0700

    getting dataset from kaggle

--- potential_proj.py
+++ potential_proj.py
@@ -1,12 +1,14 @@
+#!/usr/bin/env python3
 # test for initial commit on this file
 import pandas as pd
 import numpy as np
-import os
+import os as os
 from kaggle.api.kaggle_api_extended import KaggleApi
 
-os.environ['KAGGLE_CONFIG_DIR'] = '/Users/kerouac/Desktop/project_work/programming_projects/kaggle.json'
 
 # Initialize the Kaggle API client
 api = KaggleApi()
 api.authenticate()  # This will use your credentials from the KAGGLE_CONFIG_DIR
 
+dataset_name = 'nelgiriyewithana/top-spotify-songs-2023'
+api.dataset_download_files(dataset_name, path="/Users/kerouac/Desktop/programming_projects/", unzip=True)
