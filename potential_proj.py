865d1006612cedede99a4053364fad0aa2962c7e
bransonenani <bransonenani@ucsb.edu>
2023-10-04 17:10:21 -0700

    made some slight mods, still not working

--- potential_proj.py
+++ potential_proj.py
@@ -1,3 +1,12 @@
 # test for initial commit on this file
 import pandas as pd
 import numpy as np
+import os
+from kaggle.api.kaggle_api_extended import KaggleApi
+
+os.environ['KAGGLE_CONFIG_DIR'] = '/Users/kerouac/Desktop/project_work/programming_projects/kaggle.json'
+
+# Initialize the Kaggle API client
+api = KaggleApi()
+api.authenticate()  # This will use your credentials from the KAGGLE_CONFIG_DIR
+
